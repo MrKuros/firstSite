8547a73659b971a9d0d4ab4a0c4d2e9151b024fa
Kashish Patel <patelkashishpatel032@gmail.com>
2022-08-13 16:05:21 +0530

    Updates

---
+++ precache-manifest.d147fd200e6b3b99a921c30a674ac946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d2950c9f50d3391388b",
+    "url": "/static/css/main.5cda3408.chunk.css"
+  },
+  {
+    "revision": "7d2950c9f50d3391388b",
+    "url": "/static/js/main.7d2950c9.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "593cbd7b9335289c952b",
+    "url": "/static/js/2.593cbd7b.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7f3d0a3fcb30ade3951c15ad1aa301fe",
+    "url": "/index.html"
+  }
+];
